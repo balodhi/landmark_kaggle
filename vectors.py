2f48183975a07821de072b64999867d24f5805c2
balodhi <balodhi@gmail.com>
2018-05-01 15:46:00 +0900

    vectors.py

--- vectors.py
+++ vectors.py
@@ -7,6 +7,7 @@ import dataload_files as dataload
 import path as path_cfg
 import numpy as np
 import pickle
+from tqdm import tqdm
 
 
 def data_caller(test_dir, label_file):
@@ -20,18 +21,21 @@ def data_caller(test_dir, label_file):
     return test_loader
 
 
-def test(test_loader):
+def test(test_loader,save_folder):
     total_vec_list = []
 
     for model in modelset:
+        print('using model: ',str(model))
         checkpoint_path = os.path.join(check_point_dir, 'resnet18_' + model + '.pth.tar')
         CNN_model, _, _, _ = models.test_model_loader(checkpoint_path, model_name, dropouts=True)
 
         # switch to evaluate mode
         CNN_model.eval()
+        print('length: ',str(len(test_loader)))
 
         vec_list = []
-        for i, (img, label) in enumerate(test_loader):
+        for data in tqdm(test_loader):
+            img, label = data
             label = label[0]
             img = Variable(img).cuda()
 
@@ -42,21 +46,27 @@ def test(test_loader):
             #break
 
         # write all output to the file
-        with open((str(model) + '_output.pickle'), 'wb') as file:
+        with open(os.path.join(save_folder, (str(model) + '_output.pickle')), 'wb') as file:
             pickle.dump(vec_list, file)
 
 test_batch_size = 1
 model_name_list = ['resnet18']
 mean = [0.4606, 0.4737, 0.4678]
 std = [0.0143, 0.0170, 0.0235]
-test_dir = '../11/AugmentedTrainingSet/'
-label_dir = './t.csv'
+test_dir1 = '../nfsdrive/landmark/FullTrainingSet/train/'
+test_dir2 = '../nfsdrive/landmark/FullTrainingSet/val/'
+label_dir1 = '../nfsdrive/landmark/FullTrainingSet/train_labels.txt'
+label_dir1 = '../nfsdrive/landmark/FullTrainingSet/val_labels.txt'
+
 check_point_dir = '../weights/checkpoints/weights/'
 modelset = ['3', '11', '21', '31', '41', '51', '61', '71', '81', '91', '101', '201', '301', '401', '501', '601', '701',
             '801', '901', '1001']
 for idx, model_name in enumerate(model_name_list):
-    test_loader = data_caller(test_dir, label_dir)
-    test(test_loader)
+    test_loader1 = data_caller(test_dir1, label_dir1)
+    test(test_loader1, '../vectors/train/')
+
+    test_loader2 = data_caller(test_dir2, label_dir2)
+    test(test_loader2, '../vectors/val/')
 '''
 f = open('vectors.pickle', 'w')
 pickle.dump(info_dict, f)
