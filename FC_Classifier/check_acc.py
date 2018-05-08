f4817a66900f9dd8c7d4a0b98be1df77177a19b7
Hwejin Jung <hwejin23@gmail.com>
2018-05-08 22:39:00 +0900

    Update check_acc.py

--- FC_Classifier/check_acc.py
+++ FC_Classifier/check_acc.py
@@ -22,6 +22,7 @@ num_classes = 14951
 batch_size = 1
 
 
+skip_images = [1, 2]
 '''
 #val_dataset = dt.dataload_val('/hdd1/data_set/val/', '/hdd1/data_set/sorted/sorted_val_11.txt', '/hdd1/data_set/encoded_label.pickle', 'val',5)
 val_dataset = dt.dataload_val('/media/hwejin/SSD_1/DATA/temp_pickles', 
@@ -32,15 +33,22 @@ val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
                                            shuffle=False)
 
 '''
+
+
+
+test_dataset = dt.dataload_val('../vectors/test', skip_list)
+'''
 test_dataset = dt.dataload_val('/media/hwejin/SSD_1/DATA/temp_pickles', 
 	'/media/hwejin/SSD_1/DATA/temp_pickles/sorted/sorted_val_11.txt', 
 	'/media/hwejin/SSD_1/DATA/temp_pickles/encoded_label.pickle', 'val',5)
+'''
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                            batch_size=batch_size,
                                            shuffle=False)
 
 
-f = open('/media/hwejin/SSD_1/DATA/temp_pickles/encoded_label.pickle')
+#f = open('/media/hwejin/SSD_1/DATA/temp_pickles/encoded_label.pickle')
+f = open('../vectors/encoded_label.pickle')
 encoded_label = pickle.load(f)
 f.close()
 
@@ -105,51 +113,49 @@ def test(data_loader, net, criterion):
     loop = len(data_loader)
     
     
-    skip_images = [1, 2]
     f = open('out.txt', 'w')
     f.write('id,landmarks\n')
-    for i, (images, labels) in enumerate(data_loader):
+    for i, (images) in enumerate(data_loader):
 
-    	if i in skip_images:
+    	if len(iamges.shape) == 1:
             f.write(str(idx) + ', \n')
-        else:  
-    	
-	        images = Variable(images.view(-1, 14951)).cuda()
-	        labels = Variable(labels).cuda()
+        else:
+	    images = Variable(images.view(-1, 14951)).cuda()
+	    #labels = Variable(labels).cuda()
 
 
 
-	        outputs = net(images)
-	        numpy_output = outputs.cpu().data.numpy()
+	    outputs = net(images)
+	    numpy_output = outputs.cpu().data.numpy()
 	        
-	        for idx in range(numpy_output.shape[0]):
-	            softmax_value = __softmax(numpy_output[idx])
-                max_value = np.max(softmax_value)
-                max_label = np.argmax(softmax_value)
-                converted_label = __label_convert(encoded_label, max_label)
-                f.write(str(idx) + ','+ str(converted_label) + ' ' + str(max_value) + '\n')
-	        loss = criterion(outputs, labels)
+	    for idx in range(numpy_output.shape[0]):
+	        softmax_value = __softmax(numpy_output[idx])
+            max_value = np.max(softmax_value)
+            max_label = np.argmax(softmax_value)
+            converted_label = __label_convert(encoded_label, max_label)
+            f.write(str(idx) + ','+ str(converted_label) + ' ' + str(max_value) + '\n')
+	    #loss = criterion(outputs, labels)
 
-	        prec1, prec1 = tools.Accuracy(outputs, labels, topk=(1, 1))
-	        losses.update(loss.data[0], images.size(0))
-	        acc.update(prec1[0], images.size(0))
+	    #prec1, prec1 = tools.Accuracy(outputs, labels, topk=(1, 1))
+	    #losses.update(loss.data[0], images.size(0))
+	    #acc.update(prec1[0], images.size(0))
 
 
     #if (i + 1) % 10 == 0:
-    print('Step [%d], Loss: %.4f' % (i + 1, loss.data[0]))
-    print('Accuracy : ', (prec1.cpu().data.numpy()[0]))
-    print('Accuracy (All): ', acc.avg.cpu().data.numpy()[0])
-    print('Losses (All): ', losses.avg)
-    print()
+    #print('Step [%d], Loss: %.4f' % (i + 1, loss.data[0]))
+    #print('Accuracy : ', (prec1.cpu().data.numpy()[0]))
+    #print('Accuracy (All): ', acc.avg.cpu().data.numpy()[0])
+    #print('Losses (All): ', losses.avg)
+    #print()
     f.close()
-    return acc.avg
+    return 1
 
 
 
 def validation_run():
     net = Net.Net(input_size, hidden_size, num_classes)
-    #net = load_model('./model2.pkl',net)
-    net = load_model('/media/hwejin/SSD_1/DATA/temp_pickles/model2.pkl',net)
+    net = load_model('./model2.pkl',net)
+    #net = load_model('/media/hwejin/SSD_1/DATA/temp_pickles/model2.pkl',net)
     net.cuda()
     criterion = nn.CrossEntropyLoss()
 
@@ -160,8 +166,8 @@ def validation_run():
 def test_run():
     print("test")
     net = Net.Net(input_size, hidden_size, num_classes)
-    #net = load_model('./model2.pkl',net)
-    net = load_model('/media/hwejin/SSD_1/DATA/temp_pickles/model2.pkl',net)
+    net = load_model('./model2.pkl',net)
+    #net = load_model('/media/hwejin/SSD_1/DATA/temp_pickles/model2.pkl',net)
     net.cuda()
     criterion = nn.CrossEntropyLoss()
     acc = test(test_loader, net, criterion)
